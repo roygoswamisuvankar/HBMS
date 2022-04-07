b9c45fea19103d27ef11574b6c03c13042a758d3
Suvankar <suvankarroygoswami@gmail.com>
2022-04-07 09:26:45 +0530

    v2.5

--- HBMS_System/HBMS_App/urls.py
+++ HBMS_System/HBMS_App/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
@@ -14,4 +15,14 @@ urlpatterns = [
     path('admin_home', views.admin_home, name='admin_home'),
     #admin logout
     path('adminlogout', views.adminlogout, name='adminlogout'),
+    #admin -> add employees goto addemployees functions
+    path('addemployees', views.addemployees, name='addemployees'),
+    #admin-> show employees,
+    path('show_emp', views.show_emp, name='show_emp'),
+    #admin -> employee delete,
+    path('emp_delete/<int:id>', views.emp_delete, name='emp_delete'),
+    #admin-> edit employees
+    path('emp_edit/<int:id>', views.emp_edit, name='emp_edit'),
+    #admin -> update employee records, 
+    path('emp_edit/update_emp', views.update_emp, name='update_emp'),
 ]
--- HBMS_System/HBMS_App/views.py
+++ HBMS_System/HBMS_App/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.db import connection
-from.models import admin1
+from.models import admin1, employee1
+import hashlib
+from django.db.models import Q
 
 # Create your views here.
 #first page after server on
@@ -46,3 +48,132 @@ def adminlogout(request):
     except:
         return redirect('index')
     return redirect('index')
+
+#admin -> add emlpoyees into database
+def addemployees(request):
+    if 'check_logged' in request.session:
+        current_user = request.session['check_logged']
+        data = admin1.objects.get(phone = current_user)
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            gender = request.POST.get('gender')
+            dept = request.POST.get('dept')
+            address = request.POST.get('address')
+            password = request.POST.get('password')
+            cpassword = request.POST.get('cpassword')
+            if(password == cpassword):
+                if employee1.objects.filter(email = email).count():
+                    sms = 'This email already exits!'
+                    param = {
+                        'sms' : sms,
+                        'current_user' : data,
+                    }
+                    return render(request, 'admin_home.html', param)
+                elif employee1.objects.filter(phone = phone).count():
+                    sms = 'This Phone Number already exits!'
+                    param = {
+                        'sms' : sms,
+                        'current_user' : data,
+                    }
+                    return render(request, 'admin_home.html', param)
+                else:
+                    hash = hashlib.md5(password.encode())
+                    pass1 = hash.hexdigest()
+                    save_emp = employee1(name = name, email = email, phone = phone, gender = gender, department = dept, address = address, password = pass1)
+                    save_emp.save()
+                    sms1 = 'Employee Data Saved Successfully!'
+                    param = {
+                        'sms1' : sms1,
+                        'current_user' : data,
+                    }
+                    return render(request, 'admin_home.html', param)
+            else:
+                sms = 'Password and Confirm password did not match, please try again!'
+                param = {
+                    'current_user' : data,
+                    'sms' : sms,
+                }
+                return render(request, 'admin_home.html', param)
+        return render(request, 'admin_home.html')
+    return redirect('admin')
+
+#admin -> show employee list showemp.html
+def show_emp(request):
+    if 'check_logged' in request.session:
+        current_user = request.session['check_logged']
+        data = admin1.objects.get(phone = current_user)
+        emp = employee1.objects.all()
+        param = {
+            'current_user' : data,
+            'emp' : emp,
+        }
+        return render(request, 'showemp.html', param)
+    return redirect('admin')
+
+#admin -> emlpoyees delete,
+def emp_delete(request, id):
+    if 'check_logged' in request.session:
+        current_user = request.session['check_logged']
+        data = admin1.objects.get(phone = current_user)
+        del_emp = employee1.objects.get(id = id)
+        del_emp.delete()
+        emp = employee1.objects.all()
+        sms = 'Deleted records successfully!'
+        param = {
+            'current_user' : data,
+            'emp' : emp,
+            'sms' : sms,
+        }
+        return render(request, 'showemp.html', param)
+    return redirect('admin')
+
+#admin -> emplpyees edit form
+def emp_edit(request, id):
+    if 'check_logged' in request.session:
+        current_user = request.session['check_logged']
+        data = admin1.objects.get(phone = current_user)
+        edit = employee1.objects.get(id = id)
+        param = {
+            'current_user' : data,
+            'edit' : edit,
+        }
+        return render(request, 'empedit.html', param)
+    return redirect('admin')
+
+#admin -> employee records update from empedit.html
+def update_emp(request):
+    if 'check_logged' in request.session:
+        current_user = request.session['check_logged']
+        data = admin1.objects.get(phone = current_user)
+        if request.method == 'POST':
+            id = request.POST.get('id')
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            phone = request.POST.get('phone')
+            gender = request.POST.get('gender')
+            department = request.POST.get('department')
+            address = request.POST.get('address')
+            update_data = employee1.objects.filter(id = id).update(name = name, email = email, phone = phone, gender = gender, department = department, address = address)
+            emp = employee1.objects.all()
+            if(update_data):
+                sms = 'Update records successfully!'
+                param = {
+                    'current_user' : data,
+                    'sms' : sms,
+                    'emp' : emp,
+                }
+                return render(request, 'showemp.html', param)
+            else:
+                return redirect('show_emp')
+        else:
+            sms = 'Some problems were encountered!'
+            param = {
+                'current_user' : data,
+                'sms1' : sms,
+            }
+        return render(request, 'showemp.html', param)
+    return redirect('admin')      
+
+
